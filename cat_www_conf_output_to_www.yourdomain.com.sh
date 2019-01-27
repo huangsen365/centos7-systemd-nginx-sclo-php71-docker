8b3bd8a29b8c93b632444a6f6b15602b58e902ec
root <root@ip-172-31-44-37.ap-southeast-1.compute.internal>
2019-01-27 10:30:21 +0000

    modified:   cat_www_conf_output_to_www.yourdomain.com.sh 	new file:   php-fpm_more_settings.conf 	modified:   php-fpm_www.yourdomain.com.conf

--- cat_www_conf_output_to_www.yourdomain.com.sh
+++ cat_www_conf_output_to_www.yourdomain.com.sh
@@ -1,2 +1,4 @@
 #cat www.conf > php-fpm_www.yourdomain.com.conf
-cat www.conf | sed 's/www/www.yourdomain.com/' | sed 's/\=\ apache/\=\ sshuser/g' > php-fpm_www.yourdomain.com.conf
+#cat www.conf | sed 's/www/www.yourdomain.com/' | sed 's/\=\ apache/\=\ sshuser/g' > php-fpm_www.yourdomain.com.conf
+cat www.conf | sed 's/www/www.yourdomain.com/' | sed 's/\=\ apache/\=\ sshuser/g' | sed 's/\/var\/opt\/rh\/rh-php71\/lib\/php\/session/\/var\/opt\/rh\/rh-php71\/lib\/php\/session\/www.yourdomain.com/g' | sed 's/\/var\/opt\/rh\/rh-php71\/lib\/php\/wsdlcache/\/var\/opt\/rh\/rh-php71\/lib\/php\/wsdlcache\/www.yourdomain.com/g' > php-fpm_www.yourdomain.com.conf
+cat php-fpm_more_settings.conf >> php-fpm_www.yourdomain.com.conf
