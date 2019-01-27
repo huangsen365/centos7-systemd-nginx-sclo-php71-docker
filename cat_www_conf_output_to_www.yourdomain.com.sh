e98a76ffb35f25cba94a4957604addcdf3f3f823
root <root@ip-172-31-44-37.ap-southeast-1.compute.internal>
2019-01-27 10:11:33 +0000

    modified:   cat_www_conf_output_to_www.yourdomain.com.sh 	modified:   php-fpm_www.yourdomain.com.conf

--- cat_www_conf_output_to_www.yourdomain.com.sh
+++ cat_www_conf_output_to_www.yourdomain.com.sh
@@ -1,1 +1,2 @@
-cat www.conf > php-fpm_www.yourdomain.com.conf
+#cat www.conf > php-fpm_www.yourdomain.com.conf
+cat www.conf | sed 's/www/www.yourdomain.com/' | sed 's/\=\ apache/\=\ sshuser/g' > php-fpm_www.yourdomain.com.conf
